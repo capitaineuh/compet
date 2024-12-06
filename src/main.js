5c7fe66f3a2e52b84655c5c1c4894185342ba3bf
Arnaud146 <arnaud.schmidt@outlook.fr>
2024-12-06 13:37:49 +0100

    alpine

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
-import './style.css'
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+import persist from '@alpinejs/persist';
+import focus from '@alpinejs/focus';
 
-Alpine.plugin(collapse)
+Alpine.plugin(collapse);
+Alpine.plugin(persist);
+Alpine.plugin(focus);
 
 Alpine.data('cookieConsent', () => ({
     cookiesAccepted: localStorage.getItem('cookiesAccepted') === 'true',
@@ -14,9 +17,10 @@ Alpine.data('cookieConsent', () => ({
 
     rejectCookies() {
         localStorage.setItem('cookiesAccepted', 'false');
-        this.cookiesAccepted = true; 
-    }
-}))
+        this.cookiesAccepted = true;
+    },
+}));
 
 
-Alpine.start()
+window.Alpine = Alpine;
+Alpine.start();
